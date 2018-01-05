2e6ff21b0eddfb54b1780b5d8a509467f6942f81
LiborSitek <35118303+LiborSitek@users.noreply.github.com>
2018-01-05 01:30:25 +0100

    Update articles.js

--- routes/articles.js
+++ routes/articles.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-// Brings in Article Models
+// Brings in Article Model
 let Article = require('../models/article');
 
 // Get Article
